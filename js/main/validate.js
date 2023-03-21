ff7a3cfff18040f592b1e45db03c3e3eed8ab57f
Israel Garate <israel.garate@tsoftlatam.com>
2023-03-20 21:58:34 -0300

    se agregan test unitarios

--- js/main/validate.js
+++ js/main/validate.js
@@ -35,39 +35,85 @@ function validarFormulario(data) {
   };
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const formulario = document.querySelector('.contacto');
-  const cantidad = document.querySelector('input[type="number"]');
+if (typeof document !== 'undefined') {
+  document.addEventListener('DOMContentLoaded', () => {
+    const formulario = document.querySelector('.contacto');
+    const cantidad = document.querySelector('input[type="number"]');
 
-  formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
+    formulario.addEventListener('submit', (e) => {
+      e.preventDefault();
 
-    const contacto = document.querySelector('input[name="contacto"]:checked');
-    const data = {
-      nombre: formulario.nombre.value,
-      email: formulario.email.value,
-      telefono: formulario.telefono.value,
-      mensaje: formulario.mensaje.value,
-      opciones: formulario.opciones.value,
-      cantidad: cantidad ? cantidad.value : null, // Verificar si 'cantidad' existe antes de acceder a su valor
-      contacto: contacto ? contacto.value : null, 
-      fecha: formulario.fecha.value,
-      hora: formulario.hora.value,
-    };
+      const contacto = document.querySelector('input[name="contacto"]:checked');
+      const data = {
+        nombre: formulario.nombre.value,
+        email: formulario.email.value,
+        telefono: formulario.telefono.value,
+        mensaje: formulario.mensaje.value,
+        opciones: formulario.opciones.value,
+        cantidad: cantidad ? cantidad.value : null,
+        contacto: contacto ? contacto.value : null,
+        fecha: formulario.fecha.value,
+        hora: formulario.hora.value,
+      };
 
-    const { valido, errores } = validarFormulario(data);
+      const { valido, errores } = validarFormulario(data);
 
-    const mensajeExito = document.getElementById('mensaje-exito');
-    const form = document.getElementById('contacto');
+      const mensajeExito = document.getElementById('mensaje-exito');
+      const form = document.getElementById('contacto');
 
-    if (!valido) {
-      // Ocultar el mensaje de éxito si está visible
-      mensajeExito.style.display = 'none';
-      alert('Errores:\n\n' + errores.join('\n'));
-    } else {
-      // Mostrar el div del mensaje de éxito
-      mensajeExito.style.display = 'block';
-      form.style.display = 'none';
-    }
+      if (!valido) {
+        mensajeExito.style.display = 'none';
+        alert('Errores:\n\n' + errores.join('\n'));
+      } else {
+        mensajeExito.style.display = 'block';
+        form.style.display = 'none';
+      }
+    });
   });
-});
+}
+
+function init() {
+  if (typeof document !== 'undefined') {
+    document.addEventListener('DOMContentLoaded', () => {
+      const formulario = document.querySelector('.contacto');
+      const cantidad = document.querySelector('input[type="number"]');
+
+      formulario.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const contacto = document.querySelector('input[name="contacto"]:checked');
+        const data = {
+          nombre: formulario.nombre.value,
+          email: formulario.email.value,
+          telefono: formulario.telefono.value,
+          mensaje: formulario.mensaje.value,
+          opciones: formulario.opciones.value,
+          cantidad: cantidad ? cantidad.value : null, // Verificar si 'cantidad' existe antes de acceder a su valor
+          contacto: contacto ? contacto.value : null, 
+          fecha: formulario.fecha.value,
+          hora: formulario.hora.value,
+        };
+
+        const { valido, errores } = validarFormulario(data);
+
+        const mensajeExito = document.getElementById('mensaje-exito');
+        const form = document.getElementById('contacto');
+
+        if (!valido) {
+          // Ocultar el mensaje de éxito si está visible
+          mensajeExito.style.display = 'none';
+          alert('Errores:\n\n' + errores.join('\n'));
+        } else {
+          // Mostrar el div del mensaje de éxito
+          mensajeExito.style.display = 'block';
+          form.style.display = 'none';
+        }
+      });
+    });
+  }
+}
+
+module.exports = {
+  validarFormulario,
+  init,
+};
--- js/test/validate.test.js
+++ js/test/validate.test.js
@@ -1,78 +1,85 @@
+
 const assert = require('assert');
-const { before } = require('@jest/globals');
-const { validarFormulario } = require('../main/validate.js');
+const { validarFormulario, init } = require('../main/validate.js');
+const { JSDOM } = require('jsdom');
+const { document } = (new JSDOM('')).window;
+
+global.document = document;
 
 describe('Validación de formulario', () => {
 
   let dom;
 
-  before(() => {
+  beforeAll(() => {
+    init(); // Invoca la función init() antes de realizar las pruebas
+
     dom = new JSDOM(`<!DOCTYPE html><html><head></head><body>
-      <form class="contacto" action="" id="contacto">
-      <fieldset>
-        <legend>Información Personal</legend>
-        <label for="nombre">Nombre: </label>
-        <input type="text" id="nombre" placeholder="Tu Nombre">
+    <form class="contacto" action="" id="contacto">
+    <fieldset>
+      <legend>Información Personal</legend>
+      <label for="nombre">Nombre: </label>
+      <input type="text" id="nombre" placeholder="Tu Nombre">
 
-        <label for="email">E-mail: </label>
-        <input type="email" id="email" placeholder="Tu Correo Electrónico" required>
+      <label for="email">E-mail: </label>
+      <input type="email" id="email" placeholder="Tu Correo Electrónico" required>
 
-        <label for="telefonoId">Teléfono</label>
-        <input type="tel" id="telefonoId" placeholder="+569" required>
+      <label for="telefonoId">Teléfono</label>
+      <input type="tel" id="telefonoId" placeholder="+569" required>
 
-        <label for="mensaje">Mensaje: </label>
-        <textarea id="mensaje" cols="60" rows="10"></textarea>
+      <label for="mensaje">Mensaje: </label>
+      <textarea id="mensaje" cols="60" rows="10"></textarea>
 
-      </fieldset>
+    </fieldset>
 
-      <fieldset>
+    <fieldset>
 
-        <legend>Información Sobre Propiedad</legend>
+      <legend>Información Sobre Propiedad</legend>
 
-        <label for="opciones">Vende o Compra</label>
-        <select id="opciones">
-          <option value="" disabled selected>-- Seleccione --</option>
-          <option value="Compra">Compra</option>
-          <option value="Vende">Vende</option>
-        </select>
-        <label for="cantidad">Cantidad</label>
-        <input type="number" min="0" max="100" step="5">
+      <label for="opciones">Vende o Compra</label>
+      <select id="opciones">
+        <option value="" disabled selected>-- Seleccione --</option>
+        <option value="Compra">Compra</option>
+        <option value="Vende">Vende</option>
+      </select>
+      <label for="cantidad">Cantidad</label>
+      <input type="number" min="0" max="100" step="5">
 
-      </fieldset>
+    </fieldset>
 
-      <fieldset>
-        <legend>Contacto</legend>
+    <fieldset>
+      <legend>Contacto</legend>
 
-        <p>Como desea ser contactado:</p>
+      <p>Como desea ser contactado:</p>
 
-        <div class"forma-contacto">
-          <label for"telefono">Teléfono</label>
-          <input type="radio" name="contacto" value="telefono" id="telefono">
+      <div class"forma-contacto">
+        <label for"telefono">Teléfono</label>
+        <input type="radio" name="contacto" value="telefono" id="telefono">
 
-          <label for="correo">E-mail</label>
-          <input type="radio" name="contacto" value="correo" id="correo">
-        </div>
-        <p>Si eligió Teléfono, elija fecha y la hora</p>
-        <label for="fecha">Fecha:</label>
-        <input type="date" id="fecha">
-
-        <label for="hora">Hora:</label>
-        <input type="time" name="hora" min="09:00" max="18:00">
-
-      </fieldset>
-
-      <div id="errores"></div>
-      
-      <input type="submit" value="enviar" class="boton boton-verde">
-
-      <div id="mensaje-exito" class="mensaje-exito" style="display: none;">
-        Formulario enviado correctamente.
+        <label for="correo">E-mail</label>
+        <input type="radio" name="contacto" value="correo" id="correo">
       </div>
+      <p>Si eligió Teléfono, elija fecha y la hora</p>
+      <label for="fecha">Fecha:</label>
+      <input type="date" id="fecha">
 
-    </form>
-      </body></html>`);
+      <label for="hora">Hora:</label>
+      <input type="time" name="hora" min="09:00" max="18:00">
+
+    </fieldset>
+
+    <div id="errores"></div>
+    
+    <input type="submit" value="enviar" class="boton boton-verde">
+
+    <div id="mensaje-exito" class="mensaje-exito" style="display: none;">
+      Formulario enviado correctamente.
+    </div>
+
+  </form>
+    </body></html>>`);
   });
 
+  // Test 1
   it('debería devolver un objeto con la propiedad "valido" igual a true si todos los campos son válidos', () => {
     const data = {
       nombre: 'John Doe',
@@ -91,6 +98,7 @@ describe('Validación de formulario', () => {
     assert.strictEqual(resultado.errores.length, 0);
   });
 
+  // Test 2
   it('debería devolver un objeto con la propiedad "valido" igual a false si algunos campos están vacíos', () => {
     const data = {
       nombre: '',
@@ -114,6 +122,7 @@ describe('Validación de formulario', () => {
     assert.ok(resultado.errores.includes('Debes seleccionar una opción en Vende o Compra.'));
   });
 
+  // Test 3
   it('debería devolver un objeto con la propiedad "valido" igual a false si el formato del correo electrónico es inválido', () => {
     const data = {
       nombre: 'John Doe',
@@ -133,60 +142,4 @@ describe('Validación de formulario', () => {
     assert.ok(resultado.errores.includes('El campo E-mail tiene un formato inválido.'));
   });
 
-  it('debería devolver un objeto con la propiedad "valido" igual a false si el campo Teléfono no tiene un formato válido', () => {
-    const data = {
-      nombre: 'John Doe',
-      email: 'john@example.com',
-      telefono: 'abc12345',
-      mensaje: 'Este es un mensaje de prueba.',
-      opciones: 'Compra',
-      contacto: 'correo',
-      fecha: '2023-03-16',
-      hora: '09:00'
-    };
-
-    const resultado = validarFormulario(data);
-
-    assert.strictEqual(resultado.valido, false);
-    assert.strictEqual(resultado.errores.length, 1);
-    assert.ok(resultado.errores.includes('El campo Teléfono debe ser un número que comience con '));
-  });
-
-  it('debería devolver un objeto con la propiedad "valido" igual a false si se selecciona "contacto por teléfono" pero no se especifica la fecha y hora', () => {
-    const data = {
-      nombre:  'John Doe',
-      email: 'john@example.com',
-      telefono: '12345678',
-      mensaje: 'Este es un mensaje de prueba.',
-      opciones: 'Compra',
-      contacto: 'telefono',
-      fecha: '',
-      hora: ''
-    };
-    
-    const resultado = validarFormulario(data);
-    
-    assert.strictEqual(resultado.valido, false);
-    assert.strictEqual(resultado.errores.length, 1);
-    assert.ok(resultado.errores.includes('Debes seleccionar una fecha y hora para ser contactado por teléfono.'));
-  });
-
-  it('debería devolver un objeto con la propiedad "valido" igual a false si se selecciona una opción inválida en el campo "Vende o Compra"', () => {
-  const data = {
-  nombre: 'John Doe',
-  email: 'john@example.com',
-  telefono: '12345678',
-  mensaje: 'Este es un mensaje de prueba.',
-  opciones: 'Opción inválida',
-  contacto: 'correo',
-  fecha: '2023-03-16',
-  hora: '09:00'
-  };const resultado = validarFormulario(data);
-
-  assert.strictEqual(resultado.valido, false);
-  assert.strictEqual(resultado.errores.length, 1);
-  assert.ok(resultado.errores.includes('Debes seleccionar una opción en Vende o Compra.'));
-  });
-  });
-  
-  
+});
